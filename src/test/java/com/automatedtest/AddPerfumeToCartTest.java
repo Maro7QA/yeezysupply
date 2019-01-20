1321d78edf7ef984af3abf5aaaf0fff65a5b5156
hoffmmai <martin.hoffmeister@adidas.com>
2019-01-20 20:11:35 +0100

    fixed typos

--- src/test/java/com/automatedtest/AddPerfumeToCartTest.java
+++ src/test/java/com/automatedtest/AddPerfumeToCartTest.java
@@ -7,12 +7,12 @@ import org.junit.runner.RunWith;
 // Test class to link feature files and test implementation
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/com/automatedtest/Add_perfum_to_cart.feature"},
+@CucumberOptions(features = {"src/test/resources/com/automatedtest/Add_perfume_to_cart.feature"},
         plugin = {"pretty",
         "json:target/cucumber_json_reports/user-journey.json",
         "html:target/user-journey-html"},
         glue = {"com.automatedtest.infrastructure.driver",
                 "com.automatedtest.userjourney"})
 
-public class AddPerfumToCartTest {
+public class AddPerfumeToCartTest {
 }
